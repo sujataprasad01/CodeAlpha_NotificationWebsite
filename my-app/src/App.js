840e13b421728aeb6d2b14a9d156a1d7afe32ec9
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-11 13:50:32 +0530

    react setup and started app.js

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {
+  ChakraProvider,
+  theme,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Button,
+  Container,
+  Box
+} from '@chakra-ui/react';
+
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+     
+    </ChakraProvider>
+  );
+}
+
+export default App;
