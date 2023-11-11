ff4e432cc216ed3b294c7f020d28f0710224e934
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-11 14:06:59 +0530

    Notification section done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,13 +11,57 @@ import {
   Box
 } from '@chakra-ui/react';
 
-
+function notifyUser(notificationText="Thank you for enabling notifications!"){
+  if(!("Notification" in window)){
+    alert("Browser does not support notifications");
+  }
+  else if(Notification.permission==='granted'){
+    const notification=new Notification(notificationText);
+  } else if(Notification.permission!=='denied'){
+    Notification.requestPermission().then((permission)=>{
+      if(permission==='granted'){
+        const notification=new Notification(notificationText);
+      }
+    })
+  }
+}
 function App() {
-  return (
+  const [userResponded, setUserResponded]= useState(false);
+
+  function enableNotifsAndClsoe(){
+
+  }
+
+ function disableNotifsAndClsoe(){
+    
+  }
+
+  return(!(userResponded)&& !(Notification.permission==='granted'))? (
     <ChakraProvider theme={theme}>
-     
+     <Container>
+      <Alert status='success'>
+        <AlertIcon/>
+        <Box>
+          <AlertTitle>Notifications</AlertTitle>
+          <AlertDescription>Would you like to enable notifications?</AlertDescription>
+        </Box>
+        <Button colorScheme='teal' size='sm' onClick={enableNotifsAndClsoe}>
+          Sure!
+        </Button>
+        <Button colorScheme='gray' size='sm' onClick={disableNotifsAndClsoe}>
+          No thanks!
+        </Button>
+      </Alert>
+     </Container>
     </ChakraProvider>
-  );
+  ): (Notification.permission==='granted')? (
+    <ChakraProvider theme={theme}>
+
+    </ChakraProvider>
+  ):
+  <>
+  <h1>You have disablede notifications</h1>
+  </>
 }
 
 export default App;
